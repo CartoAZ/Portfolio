655102360af6aa4b9d289efe5c9437361d472fa0
Alex Zarley <azarley@wisc.edu>
2016-06-09 10:35:21 -0500

    Changed color of link titles and switched incorrect links; changed font size of link titles

--- js/main.js
+++ js/main.js
@@ -3,6 +3,21 @@ window.onload = setPage();
 
 
 function setPage(){
+
+    //jumbotron div
+    var jumbotron = d3.select("body").append("div")
+        .attr("class", "jumbotron")
+
+    //jumbotron container div
+    var jumbotronContainer = jumbotron.append("div")
+        .attr("class", "container")
+
+    var nameHeader = jumbotronContainer.append("h2")
+        .html("Alex Zarley")
+
+    var professions = jumbotronContainer.append("p")
+        .html("Cartographer | GIS | Archaeologist")
+
     //create div container to hold all images
     var container = d3.select("body").append("div")
         .attr("class", "container")
@@ -11,27 +26,53 @@ function setPage(){
     var row = container.append("div")
         .attr("class", "row")
 
-    //first image in folder
+    //silk road map
     var thumbnail1 = '../assets/Maps/silk-road-zarley-thumbnail.png'
     var path1 = '../assets/Maps/silk-road-zarley.png'
     var imgTitle1 = "Ancient Silk Road"
-    //create
     createImg(row, thumbnail1, path1, imgTitle1)
 
-    //first image in folder
+    //vaccination map
     var thumbnail2 = '../assets/Maps/ca_kindergarten_vac_zarley-thumbnail.png'
     var path2 = '../assets/Maps/ca_kindergarten_vac_zarley.png'
     var imgTitle2 = "Kindergarten Vaccinations in CA, 2014-2015"
-    //create
     createImg(row, thumbnail2, path2, imgTitle2)
 
-    //first image in folder
+    //CVI interactive map
     var thumbnail3 = '../assets/Maps/USGS-CVI-zarley-thumbnail.png'
     var path3 = 'http://alexzarley.github.io/CoastalVulnerability/'
-    var imgTitle3 = "Coastal Vulnerability"
-    //create
+    var imgTitle3 = "Coastal Vulnerability to Sea Level Rise"
     createImg(row, thumbnail3, path3, imgTitle3)
 
+    //Springsteen interactive map
+    var thumbnail8 = '../assets/Maps/springsteen-by-decade-thumbnail.png'
+    var path8 = 'http://alexzarley.github.io/springsteen-concerts-by-decade/'
+    var imgTitle8 = "Bruce Springsteen Concerts"
+    createImg(row, thumbnail8, path8, imgTitle8)
+
+    //africa map
+    var thumbnail4 = '../assets/Maps/afr-cultural-map-zarley-thumbnail.png'
+    var path4 = '../assets/Maps/afr-cultural-map-zarley.png'
+    var imgTitle4 = "Political Geography of Africa"
+    createImg(row, thumbnail4, path4, imgTitle4)
+
+    //Civil War Casualty map
+    var thumbnail5 = '../assets/Maps/civil_war_casualties_zarley-thumbnail.png'
+    var path5 = '../assets/Maps/civil_war_casualties_zarley.png'
+    var imgTitle5 = "Mostly Costly Land Battles of US Civil War"
+    createImg(row, thumbnail5, path5, imgTitle5)
+
+    //Madison Weather map
+    var thumbnail6 = '../assets/Maps/madison_weather_zarley-thumbnail.png'
+    var path6 = '../assets/Maps/madison_weather_zarley.png'
+    var imgTitle6 = "Madison Weather"
+    createImg(row, thumbnail6, path6, imgTitle6)
+
+    //NC Lighthouses
+    var thumbnail7 = '../assets/Maps/nc_lighthouses-zarley-thumbnail.png'
+    var path7 = '../assets/Maps/nc_lighthouses-zarley-thumbnail.png'
+    var imgTitle7 = "Madison Weather"
+    createImg(row, thumbnail7, path7, imgTitle7)
 
 }
 
@@ -46,27 +87,5 @@ function createImg(row, thumbnail, image, title) {
     var titleLink = imgContainer.append("h2")
         .attr("class", "portfolio-link")
         .html("<a class='post-link' href=" + image + " target='_blank'>" + title + "</a>")
-
-        //
-        // var titleLink = imgContainer
-        //     .append("<h2 class='portfolio-link'> <a class='post-link' href=" + image + ">Ancient Silk Road</a></h2>")
-
-    // var port2 = row.append("div")
-    //     .attr("class", "col-md-4")
-    //   .append("div")
-    //     .attr("class", "thumbnail portfolio-thumbnail")
-    //     .text("portfolio item #2")
-    //
-    // var port3 = row.append("div")
-    //     .attr("class", "col-md-4")
-    //   .append("div")
-    //     .attr("class", "thumbnail portfolio-thumbnail")
-    //     .text("portfolio item #3")
-    //
-    // // var silkRoadMap = map1
-
-
-
-
 
 };
